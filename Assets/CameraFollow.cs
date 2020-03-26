e9bffccc6862355a3cca46ff616c46e8551e62ef
JordanCasasRime96 <jordan.casas.rime@gmail.com>
2020-03-26 02:23:38 -0500

    Septimo avance: Plataformas moviles, plataforma que cae sin detenerse(tiene bug), rampas y cámara que se mueve con el player.

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject follow;
+    public Vector2 minCamPos, maxCamPos;
+    public float smoothTime; //EL tiempo de retardo al moverse la cámara
+
+    private Vector2 velocity;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //float posX = follow.transform.position.x; //Captura la posición X del Object PLayer
+        //float posY = follow.transform.position.y; //Captura la posición Y del Object PLayer
+        //Efecto de suavizado al moverse la cámara con el personaje
+        float posX = Mathf.SmoothDamp(transform.position.x, follow.transform.position.x, ref velocity.x, smoothTime); //Captura la posición X del Object PLayer
+        float posY = Mathf.SmoothDamp(transform.position.y, follow.transform.position.y, ref velocity.y, smoothTime);
+
+        transform.position = new Vector3(
+            Mathf.Clamp(posX, minCamPos.x, maxCamPos.x), //Limita en el eje X el movimiento de la cámara y se desplaza
+            Mathf.Clamp(posY, minCamPos.y, maxCamPos.y), //Limita en el eje Y el movimiento de la cámara y se desplaza
+            transform.position.z);
+    }
+}
---
+++ Assets/PlataformaFalling.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaFalling : MonoBehaviour
+{
+    public float fallDelay = 1f;
+    public float respawnDelay = 3f;
+
+    private Rigidbody2D rb2d;
+    private BoxCollider2D bc2d;
+    private Vector3 start;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        bc2d = GetComponent<BoxCollider2D>();
+        start = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+            Invoke("Respawn", fallDelay + respawnDelay);
+        }
+    }
+
+    void Fall() //Cuando empieza a caer
+    {
+        rb2d.isKinematic = false;
+        bc2d.isTrigger = true;
+    }
+
+    //Restaurar la plataforma a su posicion original
+    void Respawn()
+    {
+        transform.position = start; //Vuelve a la posición original
+        rb2d.isKinematic = true; //Vuelve cinematico la plataforma
+        rb2d.velocity = Vector3.zero; //Volver a la velocidad cero todo
+        bc2d.isTrigger = false; //Restaura a su posición el BoxCollider
+    }
+}
