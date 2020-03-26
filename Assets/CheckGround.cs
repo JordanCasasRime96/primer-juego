b6b6bf486027e878b9faebae0c008038fe0e94a4
JordanCasasRime96 <jordan.casas.rime@gmail.com>
2020-03-26 01:02:04 -0500

    Sexto avance: Plataforma moviles

--- Assets/CheckGround.cs
+++ Assets/CheckGround.cs
@@ -5,22 +5,40 @@ using UnityEngine;
 public class CheckGround : MonoBehaviour
 {
     private PlayerController player;
+    private Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         player = GetComponentInParent<PlayerController>();
+        rb2d = GetComponentInParent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D col) //Funciona cuando tocas primera vez
+    {
+        if(col.gameObject.tag == "Plataform")
+        {
+            rb2d.velocity = new Vector3(0f, 0f, 0f);
+            player.transform.parent = col.transform;
+            player.grounded = true;
+        }
     }
 
     private void OnCollisionStay2D(Collision2D col)
     {
         if (col.gameObject.tag == "Ground")
             player.grounded = true;
+        if (col.gameObject.tag == "Plataform")
+            player.transform.parent = col.transform;
+            player.grounded = true;
     }
 
     private void OnCollisionExit2D(Collision2D col)
     {
         if (col.gameObject.tag == "Ground")
+            player.grounded = false;
+        if (col.gameObject.tag == "Plataform")
+            player.transform.parent = null;
             player.grounded = false;
     }
 }
---
+++ Assets/PlataformaMovil.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformaMovil : MonoBehaviour
+{
+    public Transform target;
+    public float speed;
+
+    private Vector3 start, end;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if(target != null)
+        {
+            target.parent = null; //Permite que el objeto TARGET ya no sea hijo de la PLATAFORMA_MOVIL_1X1
+            start = transform.position;
+            end = target.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(target != null)
+        {
+            float fixedSpeed = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, speed);
+        }
+        if(transform.position == target.position)
+        {
+            target.position = (target.position == start) ? end : start; //Si "target.position == start" entonces usará la variable "end", o sino usuará "start"
+        }
+    }
+}
